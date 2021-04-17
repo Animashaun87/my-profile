d8732727253d1a76706bf7310df15a405f0663b4
animashaun87 <animashaunbidemi07@gmail.com>
2021-04-17 19:55:19 -0400

    adjusted my profile picture

--- src/components/about/about-me.js
+++ src/components/about/about-me.js
@@ -1,21 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import { Row, Col, Layout } from "antd";
 import colors from "../../utils/colors";
 import Bidemi from "../../utils/images/Bidemi.jpg";
 import "../../utils/styles/pages.css";
 
 function AboutMe({ about }) {
-  const [menu, setMenu] = useState(false);
-  const toggle = () => {
-    setMenu(false);
-  };
-
   return (
     <>
       <Layout className="about-area" style={{ padding: "80px 0px" }}>
         <Row className="about-top">
           <Col lg={12} sm={24} xs={24}>
-            <div style={{ padding: "50px 0px 0px 20px" }} onClick={toggle}>
+            <div style={{ padding: "50px 0px 0px 20px" }}>
               <h2 style={styles.about} className="about-title">
                 About
               </h2>
